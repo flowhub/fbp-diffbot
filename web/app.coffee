flowhub/fbp-diffbot

commit 1917c5abe7a04e29bab62a22be633a92a0e2d198

app: Check/add webhooks on startup

web/app.coffee
@@ -1,7 +1,9 @@
 fbpDiffBot = require '..'
+
 express = require 'express'
 bodyParser = require 'body-parser'
 debug = require('debug')('fbp-diffbot:app')
+bluebird = require 'bluebird'
 
 checkPr = (req, res) ->
   repo = "#{req.params.owner}/#{req.params.repo}"
@@ -53,6 +55,16 @@ exports.start = (override, callback) ->
   config = fbpDiffBot.common.getConfig override
 
   app = getApp config
-  app.listen config.port, (err) ->
-    return callback err if err
-    return callback null, app, config.port
+  listenApp = bluebird.promisify app.listen
+
+  fbpDiffBot.webhooks.ensureRepositoryHooks config
+  .then (added) ->
+    for add in added
+      debug 'added webhooks', add.data.url
+    if not added.length
+      debug 'no webhook additions needed'
+    return listenApp config.port
+  .then (rr) ->
+    app.port = config.port
+    return app
+  .nodeify callback

index.js
@@ -2,5 +2,6 @@ require('coffee-script/register');
 module.exports = {
   github: require('./src/github'),
   common: require('./src/common'),
+  webhooks: require('./src/webhooks'),
   diffbot: require('./src/diffbot')
 };

server.js
@@ -1,9 +1,9 @@
 require('coffee-script/register');
 var web = require('./web/app');
-web.start({}, function (err, app, port) {
+web.start({}, function (err, app) {
   if (err) {
     throw err;
     process.exit(1);
   }
-  console.log('fbp-diffbot server running on port', port);
+  console.log('fbp-diffbot server running on port', app.port);
 });

src/webhooks.coffee
@@ -40,6 +40,8 @@ ensureRepositoryHooks = (config) ->
     added = results.filter (r) -> r?
     return Promise.resolve added
 
+exports.ensureRepositoryHooks = ensureRepositoryHooks
+
 main = () ->
   config = common.getConfig {}
 

commit f408ba0e8f7b4a747982c742eb68066d6cedc6a0

app: Fix HTTP server not starting to listen
Broken since 1917c5abe7

web/app.coffee
@@ -60,7 +60,7 @@ exports.start = (override, callback) ->
   config = fbpDiffBot.common.getConfig override
 
   app = getApp config
-  listenApp = bluebird.promisify app.listen
+  listenApp = bluebird.promisify (port, callback) -> app.listen port, callback
 
   fbpDiffBot.webhooks.ensureRepositoryHooks config
   .then (added) ->